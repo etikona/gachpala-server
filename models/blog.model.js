e5c0c9333bf0e2a3eed83a6007654a41d352f9f3
eti kona <etikonapal@gmail.com>
2025-06-12 00:11:09 +0600

    blog model created

---
+++ models/blog.model.js
@@ -0,0 +1,37 @@
+import pool from "../db.js";
+
+const createBlog = async ({ title, content, category }) => {
+  const res = await pool.query(
+    `INSERT INTO blogs (title, content, category) VALUES ($1, $2, $3) RETURNING *`,
+    [title, content, category]
+  );
+  return res.rows[0];
+};
+
+const getAllBlogs = async (search, category) => {
+  let query = "SELECT * FROM blogs WHERE 1=1";
+  const params = [];
+
+  if (search) {
+    params.push(`%${search}%`);
+    query += ` AND title ILIKE $${params.length}`;
+  }
+  if (category) {
+    params.push(category);
+    query += ` AND category = $${params.length}`;
+  }
+
+  const res = await pool.query(query + " ORDER BY created_at DESC", params);
+  return res.rows;
+};
+
+const getBlogById = async (id) => {
+  const res = await pool.query("SELECT * FROM blogs WHERE id = $1", [id]);
+  return res.rows[0];
+};
+
+export default {
+  createBlog,
+  getAllBlogs,
+  getBlogById,
+};
--- routes/ai.route.js
+++ routes/ai.route.js
@@ -1,5 +1,4 @@
 import { Router } from "express";
-// import aiController from "../controllers/ai.controller.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
 import upload from "../middlewares/upload.middleware.js";
 import { analyzePlant } from "../controllers/ai.controller.js";
