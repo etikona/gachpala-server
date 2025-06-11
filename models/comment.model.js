cf85fdb184f7ce3cdfe708a38d05885df47dd13d
eti kona <etikonapal@gmail.com>
2025-06-12 00:13:07 +0600

    comment model created

---
+++ models/comment.model.js
@@ -0,0 +1,24 @@
+import pool from "../db.js";
+
+const addComment = async ({ userId, blogId, content }) => {
+  const res = await pool.query(
+    `INSERT INTO comments (user_id, blog_id, content) VALUES ($1, $2, $3) RETURNING *`,
+    [userId, blogId, content]
+  );
+  return res.rows[0];
+};
+
+const getCommentsByBlog = async (blogId) => {
+  const res = await pool.query(
+    `SELECT c.*, u.name AS username FROM comments c
+     JOIN users u ON c.user_id = u.id
+     WHERE c.blog_id = $1 ORDER BY c.created_at DESC`,
+    [blogId]
+  );
+  return res.rows;
+};
+
+export default {
+  addComment,
+  getCommentsByBlog,
+};
