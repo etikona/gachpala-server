0678a7375e55392c913e59deea2b806b0382b485
eti kona <etikonapal@gmail.com>
2025-06-26 01:08:36 +0600

    User registration successfull

--- models/user.model.js
+++ models/user.model.js
@@ -1,8 +1,9 @@
 import pool from "../db.js";
 
 export const createUser = async (name, email, password) => {
+  console.log("Creating user with:", { name, email, password });
   const result = await pool.query(
-    `INSERT INTO users (name, email, password) VALUES ($1, $2, $3,) RETURNING *`,
+    `INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *`,
     [name, email, password]
   );
   return result.rows[0];
