97e450ac0be7705673fd28115719d4f6c4002ee3
eti kona <etikonapal@gmail.com>
2025-06-25 22:36:13 +0600

    updated the user model

--- models/user.model.js
+++ models/user.model.js
@@ -1,9 +1,9 @@
 import pool from "../db.js";
 
-export const createUser = async (name, email, password, role) => {
+export const createUser = async (name, email, password) => {
   const result = await pool.query(
-    `INSERT INTO users (name, email, password, role) VALUES ($1, $2, $3, $4) RETURNING *`,
-    [name, email, password, role]
+    `INSERT INTO users (name, email, password) VALUES ($1, $2, $3,) RETURNING *`,
+    [name, email, password]
   );
   return result.rows[0];
 };
